e78cd89531c6dddd79874291b34d6a3670fa9d51
Jozy lopez <Jozylopez27@gmail.com>
2020-09-24 23:44:21 -0500

    starter route added / removed redundant files

---
+++ src/Routes/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <section>
+        <h2>Landing Page</h2>
+        <p>this is the landing page</p>
+      </section>
+    </>
+  );
+};
+
+export default LandingPage;
