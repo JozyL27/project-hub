2bbe9e121f19a4b4dd42cdd618ccfa7bd5609152
Jose <jozylopez27@gmail.com>
2020-09-28 15:25:45 -0500

    added react router and react router types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import LandingPage from "./Routes/LandingPage/LandingPage";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
     </div>
   );
 }
