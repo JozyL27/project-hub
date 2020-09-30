12d269e56ab4037993c94c2021fc8fd907e17fff
Jose <jozylopez27@gmail.com>
2020-09-30 11:41:30 -0500

    gql preactice

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Register.css";
 
+const GraphQlURL = "http://localhost:9000/";
+const fetchGreeting = async () => {
+  const res = await fetch(GraphQlURL, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+            query {
+                greeting
+            }
+            `,
+    }),
+  });
+  const data = res.json();
+  return data;
+};
+
 const Register = () => {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetchGreeting().then((res) => setMessage(res.data.greeting));
+  }, []);
+
   return (
     <section>
-      <p>do registration stuff</p>
+      <p>{message.length > 0 ? message : "Loading..."}</p>
     </section>
   );
 };
